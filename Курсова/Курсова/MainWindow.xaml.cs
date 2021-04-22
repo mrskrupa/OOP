d9ad932e3246b529b18b54c1b10b381634b390c2
mrskrupa <skripnik139@gmail.com>
2021-04-22 15:41:00 +0300

    OOP

---
+++ Курсова/Курсова/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Курсова
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AppContext db;
+        public MainWindow()
+        {
+            InitializeComponent();
+            db = new AppContext();
+        }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string pass = PassBox.Password.Trim();
+            string pass2 = PassBox2.Password.Trim();
+            string email = textBoxEmail.Text.Trim();
+
+            if (login.Length < 5)
+            {
+                textBoxLogin.ToolTip = "Це поле введено не коректно";
+                textBoxLogin.Background = Brushes.Red;
+            }
+            else if (pass.Length < 5)
+            {
+                PassBox.ToolTip = "Це поле введено не коректно";
+                PassBox.Background = Brushes.Red;
+            }
+            else if (pass != pass2)
+            {
+                PassBox2.ToolTip = "Це поле введено не коректно";
+                PassBox2.Background = Brushes.Red;
+            }
+            else if (email.Length < 5 || !email.Contains("@") || !email.Contains("."))
+            {
+                textBoxEmail.ToolTip = "Це поле введено не коректно";
+                textBoxEmail.Background = Brushes.Red;
+            }
+            else
+            {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+                PassBox.ToolTip = "";
+                PassBox.Background = Brushes.Transparent;
+                PassBox2.ToolTip = "";
+                PassBox2.Background = Brushes.Transparent;
+                textBoxEmail.ToolTip = "";
+                textBoxEmail.Background = Brushes.Transparent;
+
+                User user = new User(login, pass, email);
+                db.Users.Add(user);
+                db.SaveChanges();
+
+                UserPageWindow userPageWindow = new UserPageWindow();
+                userPageWindow.Show();
+                Hide();
+            }
+        }
+        private void Button_Window_Auth_Click(object sender, RoutedEventArgs e)
+        {
+            AuthWindow authWindow = new AuthWindow();
+            authWindow.Show();
+            Hide();
+        }
+    }
+}
