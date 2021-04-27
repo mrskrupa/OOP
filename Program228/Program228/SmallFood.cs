08895049f8e4fb0e255747adaecc1c32c974bfed
mrskrupa <skripnik139@gmail.com>
2021-04-27 19:13:38 +0300

    OOP

---
+++ Program228/Program228/SmallFood.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program228
+{
+    public class SmallFood : CalculateFood
+    {
+        private double _coefficient = 1.2;
+        public override double CalculateGrams()
+        {
+            if (Weight < 5)
+            {
+                _coefficient = 1.4;
+                return (Height + Weight) * Age * _coefficient;
+            }
+            return (Height + Weight) * Age * _coefficient;
+        }
+    }
+}
