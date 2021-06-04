a077f6f10f09ee373885d158f34ee7e491452bde
mrskrupa <skripnik139@gmail.com>
2021-06-04 09:41:10 +0300

    OOP

---
+++ екз.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace екзамен_2_6є
+{
+    class Program
+    {
+        static void MultiPlication(object numbers)
+        {
+            int i = (int)numbers;
+            if (i == 0)
+                return;
+
+            Thread thread = new Thread(MultiPlication);
+            thread.Start(i - 1);
+
+            Console.WriteLine($"Второй поток {i * i}");
+        }
+
+        private static void Main(string[] args)
+        {
+            MultiPlication(10);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"\t\tГлавный поток: {i * i}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
